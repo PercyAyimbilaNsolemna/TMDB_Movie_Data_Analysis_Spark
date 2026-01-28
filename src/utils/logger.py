5da9d4bb59c8524f1524451577ae5b2c383992fa
PercyAyimbilaNsolemna <percy.nsolebna@gmail.com>
2026-01-28 09:56:17 +0000

    Added logger function to handle setting up logger.

---
+++ src/utils/logger.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import logging
+
+
+def setup_logger(
+    name: str,
+    log_file: str,
+    level=logging.INFO
+):
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(formatter)
+
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(stream_handler)
+
+    return logger
