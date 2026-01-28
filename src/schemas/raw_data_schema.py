c6f6698ea28f653b4b3021564a026767af4526db
PercyAyimbilaNsolemna <percy.nsolebna@gmail.com>
2026-01-28 09:54:02 +0000

    Added schema for the raw data extracted from the API.

---
+++ src/schemas/raw_data_schema.py
@@ -0,0 +1,140 @@
+from pyspark.sql.types import *
+
+RAW_MOVIE_SCHEMA = StructType([
+
+    StructField("adult", BooleanType(), True),
+    StructField("backdrop_path", StringType(), True),
+
+    StructField(
+        "belongs_to_collection",
+        StructType([
+            StructField("backdrop_path", StringType(), True),
+            StructField("id", LongType(), True),
+            StructField("name", StringType(), True),
+            StructField("poster_path", StringType(), True),
+        ]),
+        True
+    ),
+
+    StructField("budget", LongType(), True),
+
+    StructField(
+        "credits",
+        StructType([
+            StructField(
+                "cast",
+                ArrayType(
+                    StructType([
+                        StructField("adult", BooleanType(), True),
+                        StructField("cast_id", LongType(), True),
+                        StructField("character", StringType(), True),
+                        StructField("credit_id", StringType(), True),
+                        StructField("gender", LongType(), True),
+                        StructField("id", LongType(), True),
+                        StructField("known_for_department", StringType(), True),
+                        StructField("name", StringType(), True),
+                        StructField("order", LongType(), True),
+                        StructField("original_name", StringType(), True),
+                        StructField("popularity", DoubleType(), True),
+                        StructField("profile_path", StringType(), True),
+                    ])
+                ),
+                True
+            ),
+            StructField(
+                "crew",
+                ArrayType(
+                    StructType([
+                        StructField("adult", BooleanType(), True),
+                        StructField("credit_id", StringType(), True),
+                        StructField("department", StringType(), True),
+                        StructField("gender", LongType(), True),
+                        StructField("id", LongType(), True),
+                        StructField("job", StringType(), True),
+                        StructField("known_for_department", StringType(), True),
+                        StructField("name", StringType(), True),
+                        StructField("original_name", StringType(), True),
+                        StructField("popularity", DoubleType(), True),
+                        StructField("profile_path", StringType(), True),
+                    ])
+                ),
+                True
+            )
+        ]),
+        True
+    ),
+
+    StructField(
+        "genres",
+        ArrayType(
+            StructType([
+                StructField("id", LongType(), True),
+                StructField("name", StringType(), True),
+            ])
+        ),
+        True
+    ),
+
+    StructField("homepage", StringType(), True),
+    StructField("id", LongType(), True),
+    StructField("imdb_id", StringType(), True),
+
+    StructField(
+        "origin_country",
+        ArrayType(StringType()),
+        True
+    ),
+
+    StructField("original_language", StringType(), True),
+    StructField("original_title", StringType(), True),
+    StructField("overview", StringType(), True),
+    StructField("popularity", DoubleType(), True),
+    StructField("poster_path", StringType(), True),
+
+    StructField(
+        "production_companies",
+        ArrayType(
+            StructType([
+                StructField("id", LongType(), True),
+                StructField("logo_path", StringType(), True),
+                StructField("name", StringType(), True),
+                StructField("origin_country", StringType(), True),
+            ])
+        ),
+        True
+    ),
+
+    StructField(
+        "production_countries",
+        ArrayType(
+            StructType([
+                StructField("iso_3166_1", StringType(), True),
+                StructField("name", StringType(), True),
+            ])
+        ),
+        True
+    ),
+
+    StructField("release_date", StringType(), True),
+    StructField("revenue", LongType(), True),
+    StructField("runtime", LongType(), True),
+
+    StructField(
+        "spoken_languages",
+        ArrayType(
+            StructType([
+                StructField("english_name", StringType(), True),
+                StructField("iso_639_1", StringType(), True),
+                StructField("name", StringType(), True),
+            ])
+        ),
+        True
+    ),
+
+    StructField("status", StringType(), True),
+    StructField("tagline", StringType(), True),
+    StructField("title", StringType(), True),
+    StructField("video", BooleanType(), True),
+    StructField("vote_average", DoubleType(), True),
+    StructField("vote_count", LongType(), True),
+])
